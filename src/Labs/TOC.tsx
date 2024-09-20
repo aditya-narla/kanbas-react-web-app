5ec8c3cca70da00cd21e49809817a6083aeedcd7
Aditya Narla <narla.a@northeastern.edu>
2024-09-19 21:35:42 -0400

    Assignment 1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -2,15 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function TOC() {
   return (
-    <div>
       <ul>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><a id="wd-github" href="https://github.com/aditya-narla/kanbas-react-web-app" target="_blank" rel="noreferrer">Github</a></li>
       </ul>
-      <a id="wd-github" href="https://github.com/aditya-narla/kanbas-react-web-app" target="_blank" rel="noreferrer">Github</a>
-    </div>
   );
 }
