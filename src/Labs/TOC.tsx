e904418b36ef57e8ed6f1e75d6d0e8de8aeaac6d
Aditya Narla <narla.a@northeastern.edu>
2024-10-03 19:25:14 -0400

    Assignment 2

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,9 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/aditya-narla/kanbas-react-web-app" target="_blank" rel="noreferrer" className="nav-link">
+        <a id="wd-github"
+          href="https://github.com/aditya-narla/kanbas-react-web-app"
+          target="_blank" rel="noreferrer" className="nav-link">
           My GitHub
         </a>
       </li>
